4a3733b34c74574eae7865e5130045676a617e51
RafliRasyidin <raflirasyidin20@gmail.com>
2020-12-05 22:06:32 +0700

    reformat DetailActivity.kt

--- app/src/main/java/com/rasyidin/moviesapp/ui/detail/DetailActivity.kt
+++ app/src/main/java/com/rasyidin/moviesapp/ui/detail/DetailActivity.kt
@@ -1,12 +1,19 @@
 package com.rasyidin.moviesapp.ui.detail
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.rasyidin.moviesapp.R
+import com.rasyidin.moviesapp.data.local.entity.Movie
+import com.rasyidin.moviesapp.data.local.entity.TV
+import com.rasyidin.moviesapp.data.vo.StatusResponse
 import com.rasyidin.moviesapp.ui.ViewModelFactory
 import com.rasyidin.moviesapp.utils.ConstantValue
 import kotlinx.android.synthetic.main.activity_detail.*
@@ -19,9 +26,14 @@ class DetailActivity : AppCompatActivity(), KodeinAware {
     override val kodein by kodein()
     private val viewModelFactory: ViewModelFactory by instance()
     private lateinit var viewModel: DetailViewModel
+    private var movie: Movie? = null
+    private var tv: TV? = null
+    private var type: String? = null
+    private var menu: Menu? = null
 
     companion object {
-        const val EXTRA_DETAIL = "extra_detail"
+        const val EXTRA_TV = "extra_tv"
+        const val EXTRA_MOVIE = "extra movie"
         const val EXTRA_TYPE = "extra_type"
         const val TYPE_MOVIE = "movie"
         const val TYPE_TV = "tv"
@@ -32,8 +44,9 @@ class DetailActivity : AppCompatActivity(), KodeinAware {
         setTheme(R.style.Theme_MoviesApp)
         setContentView(R.layout.activity_detail)
 
-        val id = intent.getIntExtra(EXTRA_DETAIL, 0)
-        val type = intent.getStringExtra(EXTRA_TYPE)
+        tv = intent?.getParcelableExtra(EXTRA_TV)
+        movie = intent.getParcelableExtra(EXTRA_MOVIE)
+        type = intent.getStringExtra(EXTRA_TYPE)
 
         viewModel = ViewModelProvider(
             this,
@@ -42,69 +55,140 @@ class DetailActivity : AppCompatActivity(), KodeinAware {
 
         if (type == TYPE_MOVIE) {
             supportActionBar?.title = "Detail Movie"
-            showMovieDetail(id)
+            showMovieDetail(movie)
 
         } else if (type == TYPE_TV) {
             supportActionBar?.title = "Detail Tv"
-            showTvDetail(id)
+            showTvDetail(tv)
         }
 
     }
 
-    private fun showMovieDetail(movieId: Int) {
-        showProgressBar()
-        viewModel.getDetailMovie(movieId).observe(this, {
-            tv_title_detail.text = it.title
-            tv_vote_count.text = it.voteCount.toString()
-            tv_overview.text = it.overview
-            tv_popularity.text = it.popularity.toString()
-            tv_release_detail.text = it.releaseDate
-            tv_score.text = it.voteAverage.toString()
-            tv_genres.text = it.genres[0].name
-            Glide.with(this)
-                .load(ConstantValue.BASE_URL_IMAGE + it.posterPath)
-                .apply {
-                    RequestOptions.placeholderOf(R.drawable.ic_loading)
-                        .error(R.drawable.ic_broken_image_black)
-                }
-                .into(img_detail)
-            Glide.with(this)
-                .load(ConstantValue.BASE_URL_IMAGE + it.backdropPath)
-                .apply {
-                    RequestOptions.placeholderOf(R.drawable.ic_loading)
-                        .error(R.drawable.ic_broken_image_black)
-                }
-                .into(img_backdrop)
-        })
-        hideProgressBar()
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_detail, menu)
+        this.menu = menu
+        if (type == TYPE_MOVIE) {
+            favoriteState(viewModel.isFavoritedMovie(movie))
+
+        } else if (type == TYPE_TV) {
+            favoriteState(viewModel.isFavoritedTv(tv))
+        }
+
+        return super.onCreateOptionsMenu(menu)
     }
 
-    private fun showTvDetail(tvId: Int) {
-        showProgressBar()
-        viewModel.getDetailTv(tvId).observe(this, {
-            tv_title_detail.text = it.name
-            tv_vote_count.text = it.voteCount.toString()
-            tv_overview.text = it.overview
-            tv_popularity.text = it.popularity.toString()
-            tv_release_detail.text = it.firstAirDate
-            tv_score.text = it.voteAverage.toString()
-            tv_genres.text = it.genres[0].name
-            Glide.with(this)
-                .load("https://image.tmdb.org/t/p/w500${it.posterPath}")
-                .apply {
-                    RequestOptions.placeholderOf(R.drawable.ic_loading)
-                        .error(R.drawable.ic_broken_image_black)
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.action_set_fav) {
+            if (type == TYPE_MOVIE) {
+                if (viewModel.isFavoritedMovie(movie)) {
+                    movie?.let { viewModel.removeFavMovie(it) }
+                    Toast.makeText(this, "Removed from Favorite", Toast.LENGTH_SHORT).show()
+                    favoriteState(false)
+                } else {
+                    movie?.let { viewModel.setFavMovie(it) }
+                    Toast.makeText(this, "Favorite", Toast.LENGTH_SHORT).show()
+                    favoriteState(true)
                 }
-                .into(img_detail)
-            Glide.with(this)
-                .load("https://image.tmdb.org/t/p/w500${it.backdropPath}")
-                .apply {
-                    RequestOptions.placeholderOf(R.drawable.ic_loading)
-                        .error(R.drawable.ic_broken_image_black)
+
+            } else if (type == TYPE_TV) {
+                if (viewModel.isFavoritedTv(tv)) {
+                    tv?.let { viewModel.removeFavTv(it) }
+                    Toast.makeText(this, "Removed from Favorite", Toast.LENGTH_SHORT).show()
+                    favoriteState(false)
+                } else {
+                    tv?.let { viewModel.setFavTv(it) }
+                    Toast.makeText(this, "Favorited", Toast.LENGTH_SHORT).show()
+                    favoriteState(true)
                 }
-                .into(img_backdrop)
+
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun favoriteState(state: Boolean) {
+        if (menu == null) return
+        val menuItem = menu?.findItem(R.id.action_set_fav)
+        if (state) {
+            menuItem?.icon = ContextCompat.getDrawable(this, R.drawable.ic_baseline_favorite_24)
+        } else {
+            menuItem?.icon =
+                ContextCompat.getDrawable(this, R.drawable.ic_baseline_favorite_border_24)
+        }
+    }
+
+    private fun showMovieDetail(movie: Movie?) {
+        viewModel.getDetailMovie(movie?.id).observe(this, {
+            when (it.status) {
+                StatusResponse.LOADING -> showProgressBar()
+                StatusResponse.SUCCESS -> {
+                    hideProgressBar()
+                    tv_title_detail.text = it.body?.title
+                    tv_vote_count.text = it.body?.voteCount.toString()
+                    tv_overview.text = it.body?.overview
+                    tv_popularity.text = it.body?.popularity.toString()
+                    tv_release_detail.text = it.body?.releaseDate
+                    tv_score.text = it.body?.voteAverage.toString()
+                    tv_genres.text = it.body?.genres?.get(0)?.name
+                    Glide.with(this)
+                        .load(ConstantValue.BASE_URL_IMAGE + it.body?.posterPath)
+                        .apply {
+                            RequestOptions.placeholderOf(R.drawable.ic_loading)
+                                .error(R.drawable.ic_broken_image_black)
+                        }
+                        .into(img_detail)
+                    Glide.with(this)
+                        .load(ConstantValue.BASE_URL_IMAGE + it.body?.backdropPath)
+                        .apply {
+                            RequestOptions.placeholderOf(R.drawable.ic_loading)
+                                .error(R.drawable.ic_broken_image_black)
+                        }
+                        .into(img_backdrop)
+                }
+                StatusResponse.ERROR -> {
+                    hideProgressBar()
+                    Toast.makeText(this, "Something mistakes", Toast.LENGTH_SHORT).show()
+                }
+            }
+
         })
-        hideProgressBar()
+    }
+
+    private fun showTvDetail(tv: TV?) {
+        viewModel.getDetailTv(tv?.id).observe(this, {
+            when (it.status) {
+                StatusResponse.LOADING -> showProgressBar()
+                StatusResponse.SUCCESS -> {
+                    hideProgressBar()
+                    tv_title_detail.text = it.body?.name
+                    tv_vote_count.text = it.body?.voteCount.toString()
+                    tv_overview.text = it.body?.overview
+                    tv_popularity.text = it.body?.popularity.toString()
+                    tv_release_detail.text = it.body?.firstAirDate
+                    tv_score.text = it.body?.voteAverage.toString()
+                    tv_genres.text = it.body?.genres?.get(0)?.name
+                    Glide.with(this)
+                        .load("https://image.tmdb.org/t/p/w500${it.body?.posterPath}")
+                        .apply {
+                            RequestOptions.placeholderOf(R.drawable.ic_loading)
+                                .error(R.drawable.ic_broken_image_black)
+                        }
+                        .into(img_detail)
+                    Glide.with(this)
+                        .load("https://image.tmdb.org/t/p/w500${it.body?.backdropPath}")
+                        .apply {
+                            RequestOptions.placeholderOf(R.drawable.ic_loading)
+                                .error(R.drawable.ic_broken_image_black)
+                        }
+                        .into(img_backdrop)
+                }
+                StatusResponse.ERROR -> {
+                    hideProgressBar()
+                    Toast.makeText(this, "Something mistakes", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+        })
     }
 
     private fun showProgressBar() {
--- app/src/main/java/com/rasyidin/moviesapp/ui/detail/DetailViewModel.kt
+++ app/src/main/java/com/rasyidin/moviesapp/ui/detail/DetailViewModel.kt
@@ -1,12 +1,46 @@
 package com.rasyidin.moviesapp.ui.detail
 
 import androidx.lifecycle.ViewModel
-import com.rasyidin.moviesapp.data.remote.repository.RemoteRepository
+import androidx.lifecycle.viewModelScope
+import com.rasyidin.moviesapp.data.local.entity.Movie
+import com.rasyidin.moviesapp.data.local.entity.TV
+import com.rasyidin.moviesapp.data.repository.MovieCatalogueRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
-class DetailViewModel(private val repository: RemoteRepository) : ViewModel() {
+class DetailViewModel(private val repository: MovieCatalogueRepository) : ViewModel() {
 
     fun getDetailMovie(movieId: Int?) = repository.getDetailMovie(movieId)
 
     fun getDetailTv(tvId: Int?) = repository.getDetailTV(tvId)
+
+    fun setFavMovie(movie: Movie) {
+        viewModelScope.launch(Dispatchers.IO) { repository.setFavMovie(movie) }
+    }
+
+    fun setFavTv(tv: TV) {
+        viewModelScope.launch(Dispatchers.IO) { repository.setFavTv(tv) }
+    }
+
+    fun removeFavMovie(movie: Movie) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.removeFavMovie(movie)
+        }
+    }
+
+    fun removeFavTv(tv: TV) {
+        viewModelScope.launch(Dispatchers.IO) { repository.removeFavTv(tv) }
+    }
+
+    fun isFavoritedMovie(movie: Movie?): Boolean =
+        runBlocking {
+            repository.isFavoritedMovie(movie)
+        }
+
+    fun isFavoritedTv(tv: TV?): Boolean =
+        runBlocking {
+            repository.isFavoritedTv(tv)
+        }
 
 }
